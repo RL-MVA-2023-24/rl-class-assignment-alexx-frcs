7243f8060584022d76c97af184052d828971563a
alexx-frcs <alex.frcs22@gmail.com>
2024-02-25 00:01:32 +0100

    train

--- src/train.py
+++ src/train.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 import pickle
 import os
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+import extratrees
 
 locale.setlocale(locale.LC_ALL, 'fr_FR')  # Définir la locale en français
 
@@ -46,60 +47,6 @@ class ProjectAgent:
     def __init__(self):
         pass
 
-    def collect_samples(self, env = env, horizon = int(1e4), disable_tqdm=False, print_done_states=False):
-        s, _ = env.reset()
-        #dataset = []
-        S = []
-        A = []
-        R = []
-        S2 = []
-        D = []
-        for _ in tqdm(range(horizon), disable=disable_tqdm):
-            a = env.action_space.sample()
-            s2, r, done, trunc, _ = env.step(a)
-            #dataset.append((s,a,r,s2,done,trunc))
-            S.append(s)
-            A.append(a)
-            R.append(r)
-            S2.append(s2)
-            D.append(done)
-            if done or trunc:
-                s, _ = env.reset()
-                if done and print_done_states:
-                    print("done!")
-            else:
-                s = s2
-        S = np.array(S)
-        A = np.array(A).reshape((-1,1))
-        R = np.array(R)
-        S2= np.array(S2)
-        D = np.array(D)
-        return S, A, R, S2, D
-    
-    def train(self, iterations = 10, gamma = .9, disable_tqdm = False):
-        S,A,R,S2,D = self.collect_samples()
-        nb_samples = S.shape[0]
-        Qfunctions = []
-        nb_actions = env.action_space.n
-        SA = np.append(S,A,axis=1)
-        for iter in tqdm(range(iterations), disable=disable_tqdm):
-            if iter==0:
-                value=R.copy()
-            else:
-                Q2 = np.zeros((nb_samples,nb_actions))
-                for a2 in range(nb_actions):
-                    A2 = a2*np.ones((S.shape[0],1))
-                    S2A2 = np.append(S2,A2,axis=1)
-                    Q2[:,a2] = Qfunctions[-1].predict(S2A2)
-                max_Q2 = np.max(Q2,axis=1)
-                value = R + gamma*(1-D)*max_Q2
-            Q = ExtraTreesRegressor()
-            Q.fit(SA,value)
-            Qfunctions.append(Q)
-        return Qfunctions
-
-
-
     def act(self, observation, use_random=False):
             # print(observation)
             if use_random:
@@ -120,4 +67,5 @@ class ProjectAgent:
             print("No model to load")
             return
         with open(path, 'rb') as f:
-            self.Q = pickle.load(f)
+            self.model= pickle.load(f)
+            self.model.eval()
